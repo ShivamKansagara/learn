d2e7c8c4a472d9c2e9d985409e517b159763ee90
Shivam Kansagara <202001086@daiict.ac.in>
2023-12-20 00:40:23 +0530

    added searchtext function

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-import "./index.css"
-import Shivam from "./Components/Shivam"
-import React,{useState,useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
+import Shivam from "./Components/Shivam";
+import React, { useState, useEffect } from "react";
 import Search from "./Components/Search";
 function App() {
-  const [Image, setImage] = useState([])
-  const [term, setterm] = useState('')
-  useEffect(()=>{
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
-    .then(res => res.json())
-    .then(data=> {
-      setImage(data.hits);
-    })
-    .catch(err => console.log(err));
-  },[term]);
+  const [Image, setImage] = useState([]);
+  const [term, setterm] = useState("");
+
+  const searchtext = (text) => {
+    setterm(text);
+  };
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImage(data.hits);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
   return (
     <>
-    <br />
-    <br />
-    <Search searchText={text => setterm(text)}></Search>
-    <br />
-    <div className='flex justify-center m-2 p-2'>
-    <div className=" mx-auto">
-    <div className="grid grid-cols-3 gap-4">
-      {Image.map(image => (
-        <Shivam key = {image.id} image={image} />
-      ))}
+      <br />
+      <br />
+      <Search searchText={searchtext}></Search>
+      <br />
+      <div className="flex justify-center m-2 p-2">
+        <div className=" mx-auto">
+          <div className="grid grid-cols-3 gap-4">
+            {Image.map((image) => (
+              <Shivam key={image.id} image={image} />
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
-    </div>
-    
     </>
   );
 }
